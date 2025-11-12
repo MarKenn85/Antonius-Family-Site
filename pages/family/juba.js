d111b074d971acd176fb12bdc951843d0f39b4ff
Anthony Kelly <230720157+MarKenn85@users.noreply.github.com>
2025-11-12 17:01:03 +1030

    small updates

--- pages/family/juba.js
+++ pages/family/juba.js
@@ -2,4 +2,13 @@ window.jubaPage = {
     name: "Juba II",
     title: "King of Mauritania",
     parent: "",
-}
+    portrait: "images/portraits/jubaII.png",
+    next: "",
+    previous: "",
+    text: [
+      `
+        <p>
+        </p>
+      `
+    ]
+  };
