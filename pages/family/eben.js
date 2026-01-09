ca2926463b3ea36b79f436e36d5c2cb2ea36f6ac
Anthony Kelly <230720157+MarKenn85@users.noreply.github.com>
2026-01-09 11:51:37 +1030

    Eben's story.

--- pages/family/eben.js
+++ pages/family/eben.js
@@ -1,6 +1,6 @@
 window.ebenPage = {
     name: "Eben Antonius Tenebrus",
-    title: "Pontifex of Pluto & Brother of Kierdyn",
+    title: "Custos Sacrī Plutonis",
     parent: "aurelia",
     portrait: "images/portraits/eben.png",
     next: "kierdyn",
@@ -8,7 +8,34 @@ window.ebenPage = {
     text: [
       `
         <p>
-        This story is yet to be written.
+        As my dear, sweet, unfiltered Innana loves to remind me, I am an old woman. I have lived a full life and seen much. I have had the joy of children, grand children, and even great grand children.
+        <br>
+        <br>
+        My dear grand daughter, Aurelia, is mother to one of the sweetest young boys. Eben is such a caring and generous soul. Quite shy and reserved at first, but he shines in his own way. Aurelia has told me various stories about him, and she is so proud of her son.
+        <br>
+        <br>
+        He has been raised here, at the Antonius estate. A fact I am delighted by, as it has meant I have been a daily part of his life. From his time as a young toddler, to now as he approaches manhood, I have watched over him and doted on him as only a great grandmother could.
+        <br>
+        <br>
+        His early years were relatively unremarkable. Playing with his brother and cousins, to learning to ride under his mother's tutelage, she insisted she teach him. She said it was only right since she'd managed to defeat her brother, and his entire military council, at horse racing. I remember that day, it was thrilling to watch her streak ahead of Alessandro and take the victory.
+        <br>
+        <br>
+        But while Eben has embraced the Antonius way of life, his academic pursuits were somewhat surprising, given his heritage. His mother is outgoing and charming, and his father was first a legionaire, then a man of the people, loved and respected by all who knew him.
+        <br>
+        <br>
+        It was thought Eben would become an officer, or perhaps even a senator one day, but his desires and inclinations have led him down a different path. One his grandmother, Aemilia, has approved of and fostered with vigor.
+        <br>
+        <br>
+        With Aemilia tending to the Lares of the family, she's always had a peripheral role when it comes to our patron God, Pluto. So when Eben expressed an interest in learning about him, Aemilia was happy to educate him, guiding him in the proper rites and quiet observances by which Pluto is honored on the estate.
+        <br>
+        <br>
+        He took to the lessons quickly, so much so that he now tends to the temple and leads the family when performing rituals and sacrifices. This vocation has also lead him down a path of scholarly pursuits. While he does not take on the duties of a scribe, he is a keen reader, spending many hours in the library.
+        <br>
+        <br>
+        Some might think that this has made Eben reclusive and unapproachable. Far from it. He may be shy and take his time to get comfortable with someone, once he finds his place of comfort, he is very vocal and opinionated, as an Antonius should be.
+        <br>
+        <br>
+        Eben has been a joy to this old woman. Watching him grow and change over the years has brought me a certainty that future of our family is in good hands. People like Eben will be leading us into the future, and I see it bright and prosperous.
         </p>
       `
     ]
