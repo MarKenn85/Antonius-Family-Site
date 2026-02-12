8e8d17801f3815329707497b7f95868e1e19cd2c
Anthony Kelly <230720157+MarKenn85@users.noreply.github.com>
2026-02-12 17:19:28 +1030

    Added Quintus

--- pages/servus/quintus.js
+++ pages/servus/quintus.js
@@ -1,15 +1,32 @@
 window.quintusPage = {
     name: "Quintus",
-    title: "Servus Antonius",
+    title: "Antonius Custo & Sporting Champion",
     parent: "",
-    portrait: "images/background/emblem.png",
+    portrait: "images/portraits/quintus.png",
     next: "wilhelm",
     previous: "dane",
     text: [
-      `
-        <p>
-        This story is yet to be written.
-        </p>
-      `
+    `
+      <p>
+      I cannot speak highly enough of this fine young man. Quintus came to us years ago with a fighting spirit and a warrior’s heart.
+      <br>
+      <br>
+      He has served our family faithfully as Custo, defending many of us as we journey across the Empire. He is loyal, brave, and rather fetching—if you will permit this 'old woman' her flights of fancy.
+      </p>
+
+      <p>
+      His aptitude for combat has carried him to the Flavian Amphitheater in Rome, where he has brought great honor to himself and to our name. On several occasions he has earned the distinction of Champion of the Arena, a title not lightly won.
+      <br>
+      <br>
+      Yet the arena is not the only place in which he has found glory. The Circus Maximus likewise called to him, and he has claimed the title of Racer of the Day many times, as well as securing victories during the Neptunalia and Saturnalia celebrations.
+      </p>
+
+      <p>
+      Through all of it, he has remained humble and steadfast in his duties. The family has grown to admire and depend upon him. I recall, too, that young Zane was once quite taken with him—often leaving small gifts in his chamber.
+      <br>
+      <br>
+      Quintus has proven himself again and again to be a loyal, respectful, and spirited man, one upon whom this family may rely without hesitation. I am deeply pleased that he is part of us.
+      </p>
+    `
     ]
   };
