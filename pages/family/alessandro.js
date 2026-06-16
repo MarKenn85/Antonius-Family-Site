70c434a167739e9f4d839a11cb32b76330353cfa
Anthony Kelly <230720157+MarKenn85@users.noreply.github.com>
2026-06-16 20:36:34 +0930

    Alessandro journal entry

--- pages/family/alessandro.js
+++ pages/family/alessandro.js
@@ -6,29 +6,53 @@ window.alessandroPage = {
     parent: "iullus",
 	spouses: [],
 	children: [],
-	role: "Commander of armies and pervader of soldiers.",
+	role: "Commander of armies and indulger of soldiers.",
     portrait: "images/portraits/alessandro.png",
     next: "jaiden",
     previous: "juba",
     text: [
       `
         <p>
-        An Unwritten Chapter
+        My first grandchild from my daughter, Aemilia, Alessandro was one of those children that made your hair stand on end from the antics he got up to. Though he was also one of the most lovable characters and constantly found ways to make an old woman smile.
         <br>
         <br>
-        Not every story has yet been committed to parchment.
+        Alessandro was born in Rome itself, with some of the city's best midwives and healers to help Aemilia through her first delivery. He was, as any Antonius worth their salt is, very stubborn and refused to make his appearance for nearly a full day. His mother wasn't amused to say the least. I can still hear her shrieking at Iullus to just reach up and pull.
         <br>
         <br>
-        Some members of House Antonius still walk their paths, adding new pages with each passing season. Their histories remain unwritten not because they are forgotten, but because their tales are still unfolding.
+        After a year or so in Rome, Alessandro was brought to Ilva and raised among the rest of the Antonius family. I was immediately smitten and doted on the toddler endlessly. I am always happiest with the little ones. Aemilia and Iullus were good, loving parents, though they often had to leave him with me or another in the family for various duties they had.
         <br>
         <br>
-        When the ink is ready, this space shall be filled.
+        Though he never confided such feelings to me as he grew older, I couldn't help but sense there was a simmering resentment in him toward his parents for all the time he didn't have them. I felt for him, of course, but I also knew that this was simply the way of life. Sometimes it is unfair, and we must make the best of it.
         <br>
         <br>
-        Until then, let it stand as a reminder that our family is not merely history.
+        Once he was old enough, Alessandro began training with the head of the Antonius guard. I would sometimes sit and watch him, observing his technique and skill grow with every lesson. I have seen enough military men in my time that I knew he would follow them and become a soldier. I could see the excitement in his eyes every time he took hold of a gladius.
         <br>
         <br>
-        It is still being written.
+        But along with his training, he found another passion. Aemilia's father, like Iullus', was a deep admirer of those with beauty, and that... appreciation, was passed on to Alessandro. While nowhere near the openly lustful being another of my children would become, he was by no means shy. Often parading around the gardens with next to nothing adorning his increasingly muscular body, teasing anyone who wouldn't shy away from him.
+        <br>
+        <br>
+        As he matured, his training became more formal and he asked his father for permission to train alongside the Roman legion. Iullus was amenable, but Aemilia was more reticent. He was her firstborn and she wasn't prepared to lose him to the tragedies of war. She fought with her son several times before Iullus and I managed to calm her and help her see that regardless of our feelings and fears, this was ultimately his choice.
+        <br>
+        <br>
+        It was years before we saw him again. He had trained hard, served his required years in the armies, fighting barbarians at the borders and insurrections at home, earning great honors and commendations to eventually become an outstanding military leader. Commanding an entire legion. I remember the letters he would send home to us, telling of his victories, and the occasional defeat.
+        <br>
+        <br>
+        A dark day came, of course, when his father died. Alessandro was away at war and couldn't be reached in time. When we heard he did finally learn what happened, he didn't come home. Aemilia was worried, as I was, that her son would take the news worse than the rest of us. We still don't know how he dealt with the loss. Only that when he finally returned to us, there was no mention of it, no sign of distress or prolonged grief.
+        <br>
+        <br>
+        Later, there was a letter from others, in loyal service to the Antonii, who watched and reported anything of note that we needed to know. They warned us of potential issues and problems brewing all over the empire. We received a report that Alessandro had been caught in a compromising position with several men under his command, and that there could be consequences enough to cost him his career. Thankfully, while there were consequences, he was retained as a military reserve commander and brought home.
+        <br>
+        <br>
+        While it wasn't something he was proud of, he later admitted to me that he was glad of the outcome, as it gave him the chance to serve his family more directly as the leader of our armed forces. A part of me also thought he may have allowed himself to be caught as a result of his father's death. Military life is not just hard on those left behind.
+        <br>
+        <br>
+        Now he is home more frequently and we are glad of it, though he has many duties and is often called away from the estate. Many times he and Gage must work together to coordinate campaigns and military strategy over water. Their bond has clearly grown stronger over the years.
+        <br>
+        <br>
+        Alessandro is highly respected and deeply loved by the family. His efforts to keep us all safe and secure are greatly appreciated and acknowledged often. And while he may still enjoy indulging his appetites throughout the estate, he is also aware of himself and those around him to keep himself in check when he must.
+        <br>
+        <br>
+        It is unfortunate that Iullus didn't live to see his son return home, but I know that if he had, he would be so very proud of Alessandro, both for all his accomplishments, and as a man.
         </p>
       `
     ]
