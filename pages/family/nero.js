5e735fc80dfc920e3ac35adbfa21d387c2af8c20
Anthony Kelly <230720157+MarKenn85@users.noreply.github.com>
2026-06-20 12:56:36 +0930

    Nero journal entry

--- pages/family/nero.js
+++ pages/family/nero.js
@@ -2,22 +2,61 @@ window.neroPage = {
     text: [
       `
         <p>
-        An Unwritten Chapter
+        As father, as son. Is that the saying? I can't quite remember now.
         <br>
         <br>
-        Not every story has yet been committed to parchment.
+		Being an old woman, wisdom is mine to share, and what I share is this... Don't grow old! It is a great inconvenience.
         <br>
         <br>
-        Some members of House Antonius still walk their paths, adding new pages with each passing season. Their histories remain unwritten not because they are forgotten, but because their tales are still unfolding.
+		In any case, Nero and his father are so alike in many ways.
         <br>
         <br>
-        When the ink is ready, this space shall be filled.
+        I am not certain if anyone knows anything of Nero's mother, but I'm told the birth took place at Capri, where Marcus had built a sprawling villa for the family.
+		<br>
+        <br>
+		Like his father... oh, 'like father...' yes, that's it. Like Damion, Nero began his life filled with laughter and curiosity about the world around him.
         <br>
         <br>
-        Until then, let it stand as a reminder that our family is not merely history.
+        He would crawl around everywhere, trying to inspect all he could see, touch anything he could get his hands on. Sometimes to the delight of women with loose curls, and sometimes to his own detriment.
         <br>
         <br>
-        It is still being written.
+		Another note of wisdom... Keep weapons out of the reach of children.
+		<br>
+		<br>
+		Damion cherished his firstborn son dearly, at first. I think the euphoria of parenthood was fleeting for him, because as the young boy grew, Damion seemed to retreat from his role as parent. Damion had clearly learned well from his mother's example.
+		<br>
+		<br>
+		As a toddler, Nero had been fascinated by flame. Often he would sit, sometimes for hours, just looking at a brazier. But somewhere around the age of ten, he discovered its uses were even more fascinating to him.
+		<br>
+		<br>
+		Needless to say, the well capacities at Capri were put to the test from that point on. Even his room wasn't spared the scorch marks of fire licking the walls. And such exquisite marble walls they were. A lovely shade of pale blue.
+		<br>
+		<br>
+		When he entered his teenage years, we found he had a deep love of horses with a yearning to ride. He quickly took to lessons and was impressing his instructor. By his sixteenth name day, he was riding almost as well as any cavalry rider.
+		<br>
+		<br>
+		His education also extended to music, taking a keen interest in stringed instruments. I can still remember the piercing screech of his first lessons. Thankfully, this did not last more than a few months, although, what a few months they were.
+		<br>
+		<br>
+		By twenty, he rode and played so beautifully, he would give performances while sitting atop his favorite steed, guiding the animal to perform small feats as flourishes to his playing.
+        <br>
+        <br>
+		It was certainly, different, and also entertaining. I recall a performance at one of Rose's feasts that impressed some of the visiting talent.
+		<br>
+		<br>
+		This love of horses is not just limited to his own personal riding. He often travels to Rome and attends the chariot races. 
+        <br>
+        <br>
+		He told me years ago he dreamt of becoming a rector and taking charge of the great Circus Maximus. Directing charioteers to their marks and sending them off at terribly fast speeds.
+		<br>
+		<br>
+		Since the family's relocation to Ilva, I'm given to understand he has now moved further afield in the empire.
+        <br>
+        <br>
+		He travels all over, and even found himself someone to love and build his own family with. I've not heard much of his beloved, though I have heard whispers of, of all things, wings.
+		<br>
+		<br>
+		I know Damion is happy for him, though we both share a deep feeling of missing him. I wonder if I will see Nero again before my time comes to meet with Pluto. I hope so, for I miss his sweet laugh and curious eyes.
         </p>
       `
     ]
