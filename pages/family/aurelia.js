a496a2388e79425161a3ebc5a3e915494a0943dc
Anthony Kelly <230720157+MarKenn85@users.noreply.github.com>
2026-07-19 21:49:39 +0930

    Aurelia's page

--- pages/family/aurelia.js
+++ pages/family/aurelia.js
@@ -2,22 +2,85 @@ window.aureliaPage = {
     text: [
       `
         <p>
-        An Unwritten Chapter
+        What an unexpected joy this girl is. Aurelia was born only a few months before Iullus died and therefore never had the chance to know her father. While this tragedy no doubt left a void in her, it has not diminished her in the slightest.
         <br>
         <br>
-        Not every story has yet been committed to parchment.
+        Aurelia is perhaps one of the most vibrant people I have ever known. It seems, almost without fail, that she wakes up every day smiling, spending her days laughing with everyone around her.
         <br>
         <br>
-        Some members of House Antonius still walk their paths, adding new pages with each passing season. Their histories remain unwritten not because they are forgotten, but because their tales are still unfolding.
+        As a girl, she would parade around showering everyone around her with affection, humor, and theatrical performances that entertained. She would often announce herself, taking her imperial heritage to heart, and proclaim that wherever she happened to be was the happiest place in the world and everyone better act like it.
         <br>
         <br>
-        When the ink is ready, this space shall be filled.
+        No one could help but adore her spirit and unabating energy. Aemilia tried to instill a greater sense of decorum in her daughter, but Aurelia would have none of it.
         <br>
         <br>
-        Until then, let it stand as a reminder that our family is not merely history.
+        She told me once, "Grandmother, life is to be lived and enjoyed for all it is worth. To do otherwise is to invite everything else that isn't joy and happiness." A lovely sentiment, I just wish it were as easy to embrace as she made it seem.
         <br>
         <br>
-        It is still being written.
+        One day, as a young woman, she traveled to Rome with Gamila and Decimus. She was fascinated by the sights of the largest city in the world. Decimus later told me she was flitting from one place to another trying to see everything she could.
+        <br>
+        <br>
+        On her third day in Rome, she met a man at the great Pantheon. While a man of high stature, coming from a respected family, Marius is truly devout to the gods. Had he not been drawn into his family's business dealings, it is likely he would have become a servant of the gods.
+        <br>
+        <br>
+        She was smitten with him immediately. I cannot say I blame her, he is a striking example of masculinity and virility. Even I felt a flutter when I first saw him. It was clear he was besotted with her as well and they courted for almost a year before being wed.
+        <br>
+        <br>
+        It was over two years later before they welcomed their first child. Some were concerned for how long it took, but everyone in the family understood that this couple were simply enjoying their life together before settling.
+        <br>
+        <br>
+        They spent their time traveling around the empire as Marius quickly embraced his wife's thirst for life. In him she'd found someone who truly understood her, and he discovered an entire world of experience through her that he found infectious.
+        <br>
+        <br>
+        They both understood their respective duties, and in time they brought their first son, Eben, into the world. When he was placed into his mother's arms, his father by her side, they both wept with joy at what they had created.
+        <br>
+        <br>
+        In Eben, they saw not only a child for them to love and cherish, they saw a whole future of possibility and potential they could help shape and guide into a new, remarkable life.
+        <br>
+        <br>
+        For the next two years they basked in the wonder that was parenthood, settling in Rome to raise their son.
+        <br>
+        <br>
+        When Eben was almost 3, we received word that Aurelia was pregnant again. I was, as you might expect, delighted. But that joy was cut short as a few months later, the trio appeared at the docks of Ilva, Marius carrying Eben in one arm as he supported Aurelia off the ship.
+        <br>
+        <br>
+        I was heartbroken for them. Aurelia had miscarried and had been tended to by healers in the city. She didn't want to remain in Rome afterward and they set sail as soon as it was safe for her to travel.
+        <br>
+        <br>
+        The family gathered around their shining girl and we watched over her as she healed. During this time, Marius became more distant, reserved. He hadn't taken this loss well at all.
+        <br>
+        <br>
+        Aurelia slowly regained her strength and eventually began to resume her life. After almost a year, she announced she was pregnant once more. She was more timid about it this time, her worry for this child's wellbeing was clear.
+        <br>
+        <br>
+        She was given strict orders to take greater care of herself and was kept under close watch. Though the months were trying and frustrating at times, she saw the pregnancy to term and delivered another healthy baby boy, Kierdyn.
+        <br>
+        <br>
+        This brought the joy back to her eyes. Her spirits lifted and her smile returned in earnest. Marius also seemed to brighten, though not as fully as his wife.
+        <br>
+        <br>
+        Marius had spent the previous months becoming more and more involved with matters concerning the gods. He had prayed and made offerings to them daily, becoming more devout than ever.
+        <br>
+        <br>
+        Several years passed as the children grew up. Eventually, Aurelia moved with her family to Hispania, where Marius devoted himself to theological study and the worship of the gods. There she raised their sons to adulthood. 
+        <br>
+        <br>
+        She wrote many times of the sights and spectacles she had seen and experienced. Of how the children were developing and growing into fine young men. And of the distance growing between her and her husband.
+        <br>
+        <br>
+        While they were not unhappy as such, it was clear that Marius' focus was drawn away from family life. Eventually they agreed to remain married, but she would return with the children to Ilva. I was devastated for her. For the children. Even for Marius given where this had stemmed from.
+        <br>
+        <br>
+        I was also angry. Part of me believes that he should have remained loyal to his duties as a husband and father. But I never let Aurelia see that.
+        <br>
+        <br>
+        Now, I watch her live her life amongst family. She smiles once more and brings joy to all those around her. I can hear her laugh from my garden writing table here as I write this. Despite the loss and heartbreak in her life, she is still able to present her happiest self to the world.
+        <br>
+        <br>
+        I envy that about her. For so many of us, the ability to show the world a face that cannot help but smile is rare. We live in such complicated times that we cannot help but envy a soul that embraces life so completely.
+        <br>
+        <br>
+        Aurelia is such a bright light of love and life. We Antonii are truly blessed by the gods for having her in our lives.
         </p>
       `
     ]
