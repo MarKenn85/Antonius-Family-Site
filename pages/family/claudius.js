217f432290a499d1a9ff4f4efccf555928946b91
Anthony Kelly <230720157+MarKenn85@users.noreply.github.com>
2026-07-20 14:42:48 +0930

    Added Claudius entry

--- pages/family/claudius.js
+++ pages/family/claudius.js
@@ -2,22 +2,46 @@ window.claudiusPage = {
     text: [
       `
         <p>
-        An Unwritten Chapter
+        I must confess that have sat here for a time, contemplating what to say about Claudius. I know so little about him beyond a short descriptor and a few key facts of note. 
         <br>
         <br>
-        Not every story has yet been committed to parchment.
+        In the beginning, Claudius came to us as a Gladiator and Charioteer. We had seen him compete several times in Rome, and he is truly gifted in both sports.
         <br>
         <br>
-        Some members of House Antonius still walk their paths, adding new pages with each passing season. Their histories remain unwritten not because they are forgotten, but because their tales are still unfolding.
+        Damion, always interested in expanding our reach and influence within the empire, took it upon himself to get to know this striking combatant and ascertain his worth.
         <br>
         <br>
-        When the ink is ready, this space shall be filled.
+        It turned out that the young man had been sold in the slave markets to the Rector of the games and made to fight and race for the entertainment of the masses. 
         <br>
         <br>
-        Until then, let it stand as a reminder that our family is not merely history.
+        The conversations between him and Damion revealed a few things. Claudius is from the northern regions outside the empire. He learned to fend for himself when his family shunned him. While he wasn't outcast, he was barely acknowledged for being smaller than the average male.
         <br>
         <br>
-        It is still being written.
+        It always upsets me to think of a family turning their backs on one of their own for being considered less than, in any way. But Claudius has appeared to make peace with his past and embraced his new life.
+        <br>
+        <br>
+        After several conversations, Claudius was bought, freed, and offered a position in the family as Custos. Defending the family and our honor. Apparently he was very grateful and quickly agreed.
+        <br>
+        <br>
+        He has spent several years with us in this capacity, developing his connections among the family members and other custos. In particular, he seemed to bond with his fellow custos, Quintus, quite strongly.
+        <br>
+        <br>
+        Despite making this commitment to our family, he has been permitted to fight and race in our name. It seems that while he had been a slave, the races and gladiatorial fights had awakened a fire in him that he wished to continue to stoke.
+        <br>
+        <br>
+        Over time, the relationship between Claudius and Damion deepened. The respect Claudius has for my grandson is obvious, and Damion seems to trust Claudius implicitly.
+        <br>
+        <br>
+        I have watched the pair numerous times and it came as no surprise to me when Damion announced to the family his intent to adopt Claudius as his son.
+        <br>
+        <br>
+        There was a great celebration and proclamation. The ceremony was every bit the Roman spectacle, and Damion clearly reveled in showing off his new progeny.
+        <br>
+        <br>
+        There hasn't been much time since then and I haven't had the chance to know Claudius as much as I'd wish to. I am an old woman and I haven't many years left to me. Perhaps circumstances will change to where I can know my new great-grandson better.
+        <br>
+        <br>
+        But for now, Claudius has made our family his, and we have made him ours. I have seen a fine young man take his place in an empire that was not his by birth, and I suspect he will make as big an impact on it as it will on him.
         </p>
       `
     ]
